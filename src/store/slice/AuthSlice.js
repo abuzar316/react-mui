c323cb84bf45b7d9c8b4e5c9c515160bb60cdb25
MOHAMMAD ABUZAR <84950162+abuzar316@users.noreply.github.com>
2022-08-19 10:28:30 +0530

    Update AuthSlice.js

--- src/store/slice/AuthSlice.js
+++ src/store/slice/AuthSlice.js
@@ -1,1 +1,111 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
+import axios from 'axios';
+
+const POSTS_URL = `http://localhost:8000/api/v0/login`;
+
+
+const initialState = {
+    user: {},
+    token: null,
+    apiStatus: null,
+    message: null,
+    status: 'idle',
+    error: null
+}
+
+
+export const loginUser = createAsyncThunk('auth/fetchauth', async (data) => {
+    try {
+        // console.log("first", data)
+        const response = await axios.post(POSTS_URL, data, {
+            withCredentials: true
+        });
+        // console.log(response)
+        return response.data;
+    } catch (error) {
+        // console.log(error)
+        return error.message;
+    }
+})
+
+export const refrashAuth = createAsyncThunk('refrashauth/fetchrefrashauth', async (data) => {
+    try {
+        // console.log("first", data)
+        const response = await axios.post(`http://localhost:8000/api/v0/refrashtoken`, data, {
+            withCredentials: true
+        });
+        // console.log("response")
+        return response.data;
+    } catch (error) {
+        // console.log(error)
+        return error.message;
+    }
+});
+
+export const logout = createAsyncThunk('logout/fetchlogout', async (data) => {
+    try {
+        // console.log("first", data)
+        const response = await axios.post(`http://localhost:8000/api/v0/logout`, null, {
+            withCredentials: true
+        });
+        console.log("response", response)
+        return response.data;
+    } catch (error) {
+        // console.log(error)
+        return error.message;
+    }
+})
+
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    extraReducers: {
+        [loginUser.pending]: (state, action) => {
+            state.status = 'loading'
+        },
+        [loginUser.fulfilled]: (state, action) => {
+            state.message = action.payload.message;
+            state.apiStatus = action.payload.status;
+            state.user = action.payload.user;
+            state.token = action.payload.user.accessToken;
+            state.status = 'success'
+        },
+        [loginUser.rejected]: (state, action) => {
+            state.status = 'failed'
+        },
+        [refrashAuth.pending]: (state, action) => {
+            state.status = 'loading'
+        },
+        [refrashAuth.fulfilled]: (state, action) => {
+            state.message = action.payload.message;
+            state.apiStatus = action.payload.status;
+            state.user = action.payload.user;
+            state.token = action.payload.user.accessToken;
+            state.status = 'success'
+        },
+        [refrashAuth.rejected]: (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload.error
+        },
+        [logout.pending]: (state, action) => {
+            state.status = 'loading'
+        },
+        [logout.fulfilled]: (state, action) => {
+            state.message = action.payload.message;
+            state.apiStatus = action.payload.status;
+            state.user = null;
+            state.token = null;
+            state.status = 'success'
+        },
+        [logout.rejected]: (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload.error
+        }
+    }
+});
+
+
+
+export default authSlice.reducer;
